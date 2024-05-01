bbfd60d4650f4d2e3ce5cb4494ed29b8abc813e0
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-01 15:58:59 +0530

    signin completed partially

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -12,6 +12,10 @@ const signupBody = zod.object({
   lastname: zod.string(),
   password: zod.string().min(8),
 });
+const signinBody = zod.object({
+  email: zod.string().email(),
+  password: zod.string().min(8),
+});
 
 router.post("/signup", async (req, res) => {
   const resp = signupBody.safeParse(req.body);
@@ -19,7 +23,7 @@ router.post("/signup", async (req, res) => {
   if (!resp.success) {
     if (resp.error.issues[0].message === "Invalid email") {
       return res.status(411).json({ message: "Invalid Email" });
-    } else if (resp.error.issues.code === "too_small") {
+    } else if (resp.error.issues[0].code === "too_small") {
       return res
         .status(411)
         .json({ message: "String must contain at least 8 character(s)" });
@@ -42,10 +46,10 @@ router.post("/signup", async (req, res) => {
   // ! We dont usually store the passwords inside the DB which poses the security concerns
   // * Create entry in Mongo
   const user = await User.create({
-    email: req.body.email,
-    password: req.body.password,
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
+    email: req.body?.email,
+    password: req.body?.password,
+    firstname: req.body?.firstname,
+    lastname: req.body?.lastname,
   });
   const userId = user._id;
   // * JsonWebTokens create a token fro Auth
@@ -58,7 +62,32 @@ router.post("/signup", async (req, res) => {
 
   res.json({ message: "User created and stored!", token: token });
 });
-// router.get("/signup", (req, res) => {});
-// router.get("/signup", (req, res) => {});
+
+router.post("/signin", async (req, res) => {
+  const resz = signinBody.safeParse(req.body);
+  // * Zod Validation
+  if (!resz.success) {
+    return res.status(411).json({ message: "Incorrect creds" });
+  }
+  const user = await User.findOne({
+    email: req.body?.email,
+    password: req.body?.password,
+  });
+  if (user) {
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token: token,
+    });
+    return;
+  }
+  res.status(411).json({
+    message: "Error while Login try again, user doesnt exist/ creds invalid",
+  });
+});
 
 export default router;
