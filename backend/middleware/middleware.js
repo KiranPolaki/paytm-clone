37d12226108bd6fb416d90f3b8e375689403ef95
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-01 19:54:18 +0530

    authmiddleware implemented

---
+++ backend/middleware/middleware.js
@@ -0,0 +1,18 @@
+import { JWT_SECRET } from "../config";
+import jwt from "jsonwebtoken";
+
+function authMiddleware(req, res) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(403).json({});
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(403).json({});
+  }
+}
+export { authMiddleware };
