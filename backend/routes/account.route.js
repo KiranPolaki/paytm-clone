e40ed917909612c5c296a45c01021fe856efd4be
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-02 23:54:11 +0530

    backend completed

--- backend/routes/account.route.js
+++ backend/routes/account.route.js
@@ -10,9 +10,39 @@ router.get("/balance", async (req, res) => {
   res.json({ balance: account.balance });
 });
 
-router.post("/transfer", (req, res) => {
-  const toUser = req.body.to;
-  const amount = req.body.amount;
+router.post("/transfer", async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  const { amount, to } = req.body;
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
+  if (!account || account.balance < amount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Insufficient balance",
+    });
+  }
+  const toAccount = await Account.findOne({ userId: to }).session(session);
+  if (!toAccount) {
+    await session.abortTransaction();
+    res.status(400).json({
+      message: "Invalid account",
+    });
+  }
+  // * Performing the transfer here
+  await Account.updateOne(
+    { userId: req.userId },
+    { $inc: { balance: -amount } }
+  ).session(session);
+  await Account.updateOne(
+    { userId: to },
+    { $inc: { balance: amount } }
+  ).session(session);
+  await session.commitTransaction();
+  res.json({
+    message: "Transfer successful",
+  });
 });
 
 export default router;
