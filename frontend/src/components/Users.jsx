00067eb185086766f26c911d253225cec0a43856
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-04 22:42:11 +0530

    excluded myself from bulk

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,18 +1,31 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Button } from "./Button";
 
 export const Users = () => {
-  // Replace with backend call
-  const [users, setUsers] = useState([
-    {
-      firstName: "Harkirat",
-      lastName: "Singh",
-      _id: 1,
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/v1/user/bulk",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        console.log(response.data.users);
+        setUsers(response.data.users);
+      } catch (error) {
+        console.log("An error occured while fetching users:", error);
+      }
+    };
+    getUsers();
+  }, []);
 
   return (
     <>
@@ -26,7 +39,7 @@ export const Users = () => {
       </div>
       <div>
         {users.map((user) => (
-          <User user={user} />
+          <User key={user?._id} user={user} /> // provide a 'key' prop
         ))}
       </div>
     </>
@@ -39,12 +52,12 @@ function User({ user }) {
       <div className="flex">
         <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
           <div className="flex flex-col justify-center h-full text-xl">
-            {user.firstName[0]}
+            {user?.firstname[0]}
           </div>
         </div>
         <div className="flex flex-col justify-center h-ful">
           <div>
-            {user.firstName} {user.lastName}
+            {user?.firstname} {user?.lastname}
           </div>
         </div>
       </div>
