7eabc60a3d55f4acc26e8ce201db7c03b839326b
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-04 16:15:38 +0530

    signin and signup is completed

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,50 +1,30 @@
-export function Signup() {
+import { BottomWarning } from "../components/BottomWarning";
+import { Button } from "../components/Button";
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/InputBox";
+import { SubHeading } from "../components/SubHeading";
+
+export const Signup = () => {
   return (
-    <>
-      <div className="flex flex-col justify-center items-center bg-slate-600 text-black w-full h-screen">
-        <div className="w-80 h-max p-6 bg-white rounded-lg flex flex-col items-center justify-center">
-          <div className="pb-3">
-            <h1 className="text-center font-bold text-xl">Create an account</h1>
-            <p className="text-center text-gray-500">
-              Enter your info to enjoy seemless payments!
-            </p>
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign up"} />
+          <SubHeading label={"Enter your infromation to create an account"} />
+          <InputBox placeholder="John" label={"First Name"} />
+          <InputBox placeholder="Doe" label={"Last Name"} />
+          <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
+          <InputBox placeholder="123456" label={"Password"} />
+          <div className="pt-4">
+            <Button label={"Sign up"} />
           </div>
-          <div className="flex flex-col gap-2 p-4 w-full pb-6">
-            <input
-              placeholder="Enter your First Name"
-              className="border-2 border-indigo-950 rounded-lg p-1.5"
-            />
-            <input
-              placeholder="Enter your Last Name"
-              className="border-2 border-indigo-950 rounded-lg p-1"
-            />
-            <input
-              placeholder="Enter your Email"
-              className="border-2 border-indigo-950 rounded-lg p-1"
-            />
-            <input
-              placeholder="Enter yout Password"
-              className="border-2 border-indigo-950 rounded-lg p-1"
-            />
-            <button className="drop-shadow-lg bg-gray-900 rounded-lg p-2 text-white">
-              Signup
-            </button>
-          </div>
-          {/* <div>
-            <input type="checkbox" />
-            <div>
-              By signing up you accept the <span>Terms of services</span> and{" "}
-              <span>Privacy policy</span>
-            </div>
-          </div> */}
-          <p>
-            Already have an account?{" "}
-            <a href="signup.com" className="underline">
-              login
-            </a>
-          </p>
+          <BottomWarning
+            label={"Already have an account?"}
+            buttonText={"Sign in"}
+            to={"/signin"}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
