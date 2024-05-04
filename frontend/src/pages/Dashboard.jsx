d31b0620ba5aebd09ef885c11c3df2194b5202a0
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-04 16:23:41 +0530

    all dummy non functional code is completed

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,42 +1,15 @@
-function Dashboard() {
+import { Appbar } from "../components/Appbar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
+
+export const Dashboard = () => {
   return (
-    <div className="dashboard-container w-full h-screen flex flex-wrap p-5">
-      <div className="w-1/2 h-full flex items-center flex-col">
-        <div className="w-full h-1/5">Welcome to Paytm V01</div>
-        <div className="w-full flex gap-1 h-2/5">
-          <input type="search" className="w-9/12 h-auto" />
-          <button className="w-1/5">search</button>
-        </div>
-        <div className="flex w-full h-2/5 items-center justify-center">
-          <ul>
-            <li>
-              list <button>send</button>
-            </li>
-            <li>
-              list <button>send</button>
-            </li>
-            <li>
-              list <button>send</button>
-            </li>
-            <li>
-              list <button>send</button>
-            </li>
-            <li>
-              list <button>send</button>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="w-1/2 h-full flex items-center flex-col">
-        <div className="w- full h-1/2">
-          <p>Hello User</p>
-          <img src="" alt="logopfp"></img>
-        </div>
-        <div className="h-3/5">Dashboard</div>
-        <div className="h-1/5">balance</div>
+    <div>
+      <Appbar />
+      <div className="m-8">
+        <Balance value={"10,000"} />
+        <Users />
       </div>
     </div>
   );
-}
-
-export { Dashboard };
+};
